7e4ed9e4924bdc982114f5141d6582f26568a490
Jacob MacLeod <jmacleod142@gmail.com>
2026-04-26 17:12:25 -0400

    Load and inspect datasets

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import pandas as pd
 # Download latest version
 path = kagglehub.dataset_download("claudiodavi/superhero-set")
 
-print("Path to dataset files:", path)
+df_heroes = pd.read_csv(f"{path}/heroes_information.csv")
+df_powers = pd.read_csv(f"{path}/super_hero_powers.csv")
 
-for file in os.listdir(path):
-    print(file)
+print(df_heroes.head(20))
+print(df_powers.head(20))
