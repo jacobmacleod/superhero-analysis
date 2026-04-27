b6bc10881d81bf4d4fcbd0232f38891ea7c77476
Jacob MacLeod <macleod.6@wright.edu>
2026-04-27 10:22:20 -0400

    Remove duplicated code

--- load_data.py
+++ load_data.py
@@ -6,11 +6,3 @@ path = kagglehub.dataset_download("claudiodavi/superhero-set")
 
 df_heroes = pd.read_csv(f"{path}/heroes_information.csv")
 df_powers = pd.read_csv(f"{path}/super_hero_powers.csv")
-
-genders = ['-']
-
-print(df_heroes.columns)
-
-filtered_df = df_heroes[df_heroes['Gender'].isin(genders)]
-
-print(filtered_df[["name", "Publisher"]])
