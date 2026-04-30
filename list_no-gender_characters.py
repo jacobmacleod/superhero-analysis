e2bd524570db3737bd3657bb7b310335358733cd
Jacob MacLeod <macleod.6@wright.edu>
2026-04-30 17:00:26 -0400

    Print list of characters

--- list_no-gender_characters.py
+++ list_no-gender_characters.py
@@ -4,6 +4,6 @@ genders = ['-']
 
 filtered_df = df_heroes[df_heroes['Gender'].isin(genders)]
 
-filtered_df[["name", "Publisher"]]
+print(filtered_df[["name", "Publisher"]])
 
 print(f"There are {filtered_df.shape[0]} superheroes with no gender listed.")
